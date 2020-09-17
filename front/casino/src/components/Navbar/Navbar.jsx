d74feb752ca30736d2a12cfd9ab54254c7c430c3
Евгений Швецов <67862313+EvgenyShvetcov@users.noreply.github.com>
2020-09-17 11:51:04 +0300

    FrontEvgeny

---
+++ front/casino/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div>
+            <div>Казино "Стимулирующие Выплаты"</div>
+            <button>Личный кабинет</button>
+        </div>
+    );
+}
+
+export default Navbar;
