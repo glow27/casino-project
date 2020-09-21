6a96ad5c6f06cf4fc88a26744ca764ef6ff9d433
Albert <albpfau@gmail.com>
2020-09-21 18:08:32 +0300

    chges

--- front/casino/src/components/Login/Login.js
+++ front/casino/src/components/Login/Login.js
@@ -1,66 +1,54 @@
-import React,{useRef,useEffect, useState} from 'react'
+import React, { useRef, useEffect, useState } from "react";
 import { TweenMax, TimelineLite, Power3 } from "gsap";
 
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { userLogin } from "../../redux/actionCreator";
 
-import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { userLogin } from '../../redux/actionCreator';
+import "../../App.scss";
 
-
-import '../../App.scss'
-
-
-function Login(){
-  
+function Login() {
   const dispatch = useDispatch();
   const history = useHistory();
   const [error, setError] = useState(false);
 
-  
-
-  
-
   const handleClick = async (e) => {
-    
     e.preventDefault();
     const { email, password } = e.target;
-    const respons = await fetch('http://localhost:4000/login', {
-      method: 'POST',
+    const respons = await fetch("http://localhost:4000/login", {
+      method: "POST",
       body: JSON.stringify({
         email: email.value,
         password: password.value,
       }),
-      headers: { 'Content-type': 'Application/json' },
+      headers: { "Content-type": "Application/json" },
     });
     const user = await respons.json();
-    const {name, points, _id} = user;
+    const { name, points, _id } = user;
     console.log(user);
     if (respons.status === 200) {
-      dispatch(userLogin({ name, points, _id, auth: true }))
-      return history.push('/');
+      dispatch(userLogin({ name, points, _id, auth: true }));
+      return history.push("/");
     }
-    return setError('Ошибка!');
+    return setError("Ошибка!");
   };
 
-  
   let login = useRef(null);
-  let tl = new TimelineLite({delay: .8});
- 
+  let tl = new TimelineLite({ delay: 0.8 });
+
   useEffect(() => {
-
-
-    TweenMax.to(login, 0, { css: { visibility: "visible" } })
-   tl.from(login, 1.2, {y:1280, ease: Power3.easeOut}, 'Start')
-     .from(login, 1.5,{scale: 1.4, ease: Power3.easeOut}, .1)
-
-  },[tl]);
-  return (<>
-  <div>
-
-      <div >
-        
-        <div className="Login" ref={(el) => (login = el)}>
-          <div className="vod">
+    TweenMax.to(login, 0, { css: { visibility: "visible" } });
+    tl.from(login, 1.2, { y: 1280, ease: Power3.easeOut }, "Start").from(
+      login,
+      1.5,
+      { scale: 1.4, ease: Power3.easeOut },
+      0.1
+    );
+  }, [tl]);
+  return (
+    <div>
+      <div className="Login" ref={(el) => (login = el)}>
+        <div className="vod">
           <form onSubmit={(e) => handleClick(e)}>
             <div className="input-container">
               <input name="email" type="email" placeholder="Email" required />
@@ -68,32 +56,36 @@ function Login(){
             </div>
 
             <div className="input-container2">
-              <input name="password" type="password" placeholder="password" required/>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                required
+              />
               <i className="zmdi zmdi-lock zmdi-hc-lg"></i>
             </div>
 
-            <button className="signin"
-              type="submit"
-            >
+            <button className="signin" type="submit">
               Войти
             </button>
-            </form>
-            <div>
-              
-               <a href="http://localhost:4000/login/yandex" className="yandex" type="submit"></a>
-            </div>
-           <div>
-             
-            
-              <a href='http://localhost:4000/login/vkontakte' className="vk" type="submit"></a>
-           </div>
+          </form>
+          <div>
+            <a
+              href="http://localhost:4000/login/yandex"
+              className="yandex"
+              type="submit"
+            ></a>
+          </div>
+          <div>
+            <a
+              href="http://localhost:4000/login/vkontakte"
+              className="vk"
+              type="submit"
+            ></a>
+          </div>
         </div>
-       
       </div>
-      
     </div>
-    
-  )
- 
+  );
 }
 export default Login;
